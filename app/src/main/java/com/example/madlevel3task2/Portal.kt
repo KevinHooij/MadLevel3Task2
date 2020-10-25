9de47d258ce223659ba429a27e86e301485376a5
KevinHooij <kevin.hooijschuur@hva.nl>
2020-10-25 12:51:27 +0100

    Adds and sets up all necessary classes and xml files

---
+++ app/src/main/java/com/example/madlevel3task2/Portal.kt
@@ -0,0 +1,7 @@
+package com.example.madlevel3task2
+
+data class Portal(
+    var nameText: String,
+    var urlText: String
+
+)
---
+++ app/src/main/java/com/example/madlevel3task2/PortalAdapter.kt
@@ -0,0 +1,37 @@
+package com.example.madlevel3task2
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_portal.view.*
+
+class PortalAdapter(private val portals: List<Portal>) : RecyclerView.Adapter<PortalAdapter.ViewHolder>(){
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_portal, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return portals.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.databind(portals[position])
+    }
+
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        //  val binding = ItemReminderBinding.bind(itemView)
+
+        fun databind(portal: Portal) {
+            itemView.tvName.text = portal.nameText
+        }
+    }
+
+
+}
